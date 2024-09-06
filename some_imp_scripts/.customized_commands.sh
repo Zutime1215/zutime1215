458de9f335718e3043c40e64c67b84498c55ffaa
zahid <zahid.lmn51@gmail.com>
2024-09-06 12:16:30 +0600

    i am sirens-Fri Sep  6 12:16:30 PM +06 2024

--- some_imp_scripts/.customized_commands.sh
+++ some_imp_scripts/.customized_commands.sh
@@ -45,26 +45,36 @@ function py() {
   python3 $1 $2 $3 $4 $5
 }
 
+function shoot() {
+  sudo shutdown -P now
+}
+
 # made my life easy
 function mmle() {
   # Creating and copying important files and folder
-  mkdir mmle
-  cd mmle
+  cd /home/$(whoami)/Public/BackToTheGithub
+  mkdir $(date +%Y-%m-%d_%H:%M:%S)
+  cp -rf Zutime1215/.git ./$(date +%Y-%m-%d_%H)*
+  cd $(date +%Y-%m-%d_%H)*
+
   mkdir sublime_settings
-  cp -rf /home/$(whoami)/.config/sublime-text/Packages/User/* ./sublime_settings/
+  cp -rf /home/$(whoami)/.config/sublime-text/Packages/User/git* ./sublime_settings/
+  cp -rf /home/$(whoami)/.config/sublime-text/Packages/User/Package\ Control.sublime-settings ./sublime_settings/
   cp -rf /home/$(whoami)/Documents/some_imp_codes ./
   mkdir some_imp_scripts
   cp -rf /home/$(whoami)/.customized_commands.sh ./some_imp_scripts/
 
   # Uploading Part
-  cd ..
   git add .
-  commit_msg=$(whoami)"-"$(date)
-  git commit -m "i am $commit_msg"
+  git commit -m "i am $(whoami)"-"$(date)"
   git push -u origin main
+
+  cd ..
+  rm -rf Zutime1215
+  mv $(date +%Y-%m-%d_%H)* Zutime1215
   echo -e "\e[31m\e[1m\e[5mUploaded Boss!!!"
 }
 
 function test() {
-  cd /home/$(whoami)
+  mkdir $(date +%Y-%m-%d_%H:%M:%S)
 }
